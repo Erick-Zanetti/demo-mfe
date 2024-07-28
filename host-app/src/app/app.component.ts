475fb08357bd31837d0d1f57a8d284ef566e11a4
Erick Zanetti <erick_98zanetti_98@hotmail.com>
2024-07-28 17:56:34 -0300

    all works

--- host-app/src/app/app.component.ts
+++ host-app/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'host-app';
+
 }
--- mfe1/src/app/app.routes.ts
+++ mfe1/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Mfe1Component } from './mfe1/mfe1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Mfe1Component },
+];
